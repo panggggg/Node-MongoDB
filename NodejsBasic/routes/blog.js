a814d16613a6779d3df4d3cd409d0b5843840957
panggggg <p.picczy@gmail.com>
2021-05-10 12:07:10 +0700

    no message

--- NodejsBasic/routes/blog.js
+++ NodejsBasic/routes/blog.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const db = require('monk')("localhost:27017/BlogsDB"); //connect db
+
 
 router.get('/', function (req, res, next) {
     res.render("blog");
@@ -8,6 +10,23 @@ router.get('/', function (req, res, next) {
 router.get('/add', function (req, res, next) {
     res.render("addBlog");
 });
+
+router.post('/add', function (req, res, next) {
+    var ct = db.get('blogs');
+    ct.insert({
+        name: req.body.name,
+        description: req.body.description,
+        author: req.body.author
+    }, function (err, blog) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.location('/blog/add');
+            res.redirect('/blog/add'); //ถ้าบันทึกแล้วจะ redirect มาที่หน้า add
+        }
+    });
+});
+
 
 
 
